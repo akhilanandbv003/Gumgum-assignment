b330ae77c73d5efc0250efbdda729880f467183c
akhilanand benkal venkanna <akhilanand.venkanna@monsanto.com>
2019-04-04 19:02:00 -0500

    unit tests passing

---
+++ src/test/scala/com/example/GumgumTest.scala
@@ -0,0 +1,25 @@
+package com.example
+
+import com.github.mrpowers.spark.fast.tests.DatasetComparer
+import org.scalatest.FunSuite
+
+class GumgumTest extends FunSuite with SparkSessionTestWrapper with DatasetComparer {
+
+test("Assert nextPageUrl returns the right ​nextPageUrl"){
+  val sourceDF = spark.createDataFrame(Seq(
+    ("1","001","view","user001","abc.com"),
+    ("2","002","view","user002","xyz.com"),
+    ("3","003","view","user001","xyz.com")
+  )).toDF("id","timestamp","type","visitorId","pageUrl")
+
+  val actualDF = Gumgum.nextPageUrl(sourceDF)
+
+  val expectedDF = spark.createDataFrame(Seq(
+    ("1","001","view","user001","abc.com","xyz.com"),
+    ("2","002","view","user002","xyz.com",null),
+    ("3","003","view","user001","xyz.com",null)
+  )).toDF("id","timestamp","type","visitorId","pageUrl", "​nextPageUrl")
+
+  assertSmallDatasetEquality(actualDF, expectedDF)
+}
+}
