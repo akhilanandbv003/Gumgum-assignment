db49dbc10e8a70b313c171649926221651dbb7f1
akhilanand benkal venkanna <akhilanand.venkanna@monsanto.com>
2019-04-05 22:04:01 -0500

    added tests

--- src/main/scala/com/example/Gumgum.scala
+++ src/main/scala/com/example/Gumgum.scala
@@ -4,30 +4,37 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Gumgum {
   val spark = SparkSession
-    .builder().master("local[4]").appName("ReadFromS3")
+    .builder().master("local[*]").appName("Gumgum-assignment")
     .getOrCreate()
 
   import spark.implicits._
 
   def main(args: Array[String]): Unit = {
-//    val fileLocation = args(0)
-    val df = spark.read.json("src/main/resources/part-00000-732eeb74-f251-4f96-85d5-5c9ae95ba709-c000.txt")
-//    val df = spark.read.json(fileLocation)
-    val output = nextPageUrl(df)
-//    output.write.mode("overwrite").json(args(1))
-    output.coalesce(1).write.mode("overwrite").json("src/main/resources/output")
+    val fileLocation = args(0)
+    //    val fileLocation = "src/main/resources/part-00000-732eeb74-f251-4f96-85d5-5c9ae95ba709-c000.txt"
+
+    val inputDf = readJsonFile(spark, fileLocation)
+    val output: DataFrame = nextPageUrl(inputDf)
+    saveAsJsonFile(output, fileLocation)
+  }
+
+  def readJsonFile(spark: SparkSession, fileLocation: String) = {
+    spark.read.json(fileLocation)
   }
 
   def nextPageUrl(df: DataFrame): DataFrame = {
-    val data = df.as("e1").join(df.as("e2"), ($"e1.visitorId" <=> $"e2.visitorId") && ($"e2.timestamp" > $"e1.timestamp"), "left")
+    val data = df.as("e1").join(df.as("e2"), $"e1.visitorId" === $"e2.visitorId" && $"e2.timestamp" > $"e1.timestamp", "left") //82145
+    //    val data = df.as("e1").join(df.as("e2"), $"e1.visitorId" <=> $"e2.visitorId" && $"e2.timestamp" > $"e1.timestamp", "left") //134,231,789
+    //    val data = df.as("e1").join(df.as("e2"), $"e1.visitorId" === $"e2.visitorId", "left").filter($"e2.timestamp" > $"e1.timestamp") //22,289 same as sql
     val output = data.select($"e1.*", $"e2.pageUrl".as("nextPageUrl"))
-    output.count()
-    output.show()
     output
   }
 
-  //
-
+  def saveAsJsonFile(df: DataFrame, location: String) = {
+    df.write.json(location)
+    //    output.write.mode("overwrite").option("compression", "gzip").json(args(1))
+    //    output.coalesce(1).write.mode("overwrite").json("src/main/resources/output")
+  }
 
 
 }
--- src/test/scala/com/example/GumgumTest.scala
+++ src/test/scala/com/example/GumgumTest.scala
@@ -18,7 +18,7 @@ test("Assert nextPageUrl returns the right ​nextPageUrl"){
     ("1","001","view","user001","abc.com","xyz.com"),
     ("2","002","view","user002","xyz.com",null),
     ("3","003","view","user001","xyz.com",null)
-  )).toDF("id","timestamp","type","visitorId","pageUrl", "​nextPageUrl")
+  )).toDF("id","timestamp","type","visitorId","pageUrl","nextPageUrl")
 
   assertSmallDatasetEquality(actualDF, expectedDF)
 }
